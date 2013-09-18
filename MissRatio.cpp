a81e778b604fd1ff27f78e45462c6a790f6fd255
Ethan Poulter <ethan@cs.utexas.edu>
2013-09-18 18:28:42 -0500

    Implemented the different Matrix Multiplies
    Fixes #1. Implemented all the permutations of the loops,
    "IJK". I understand changing the size is a pain.
    If you have a way to make it better, please do so. Even if you
    must completely change the structure.

--- MissRatio.cpp
+++ MissRatio.cpp
@@ -9,12 +9,16 @@ stuff do WHATEVER YOU WANT man. This is just something to start with.
 */
 
 /* Function Declarations */
-void naiveMMM(int size);
+void IJK(int size);
+void IKJ(int size);
+void JIK(int size);
+void KIJ(int size);
+void KJI(int size);
 void fillMatrices(double matrixA[][3], double matrixB[][3], int size);
 
 
 /* Implementation of the first naive I, J, K matrix multiply */
-void naiveMMM(int size) {
+void IJK(int size) {
 
   int i,j,k;
   double sum;
@@ -42,6 +46,154 @@ void naiveMMM(int size) {
    std:: cout<<std::endl;  
    }
 }
+
+void IKJ(int size) {
+
+  int i,j,k;
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
+   for (i = 0; i < size; i++){
+        for (k = 0; k < size; k++){
+	    sum = 0;
+            for (j = 0; j < size; j++){
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+	matrixC[i][j] = sum;
+        }
+   }
+	
+   std::cout<<"The result is "<<std::endl;
+   for(i = 0; i < size; i++){
+       for( j = 0; j < size; j++){
+	  std:: cout<<matrixC[i][j]<<" ";
+	}
+   std:: cout<<std::endl;  
+   }
+}
+
+void JIK(int size) {
+
+  int i,j,k;
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
+   for (j = 0; j < size; j++){ 
+	for (i = 0; i < size; i++){
+	    sum = 0;
+            for (k = 0; k < size; k++){
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+	matrixC[i][j] = sum;
+        }
+   }
+	
+   std::cout<<"The result is "<<std::endl;
+   for(i = 0; i < size; i++){
+       for( j = 0; j < size; j++){
+	  std:: cout<<matrixC[i][j]<<" ";
+	}
+   std:: cout<<std::endl;  
+   }
+}
+
+void JKI(int size) {
+
+  int i,j,k;
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
+   for (j = 0; j < size; j++){
+        for (k = 0; k < size; k++){
+	    sum = 0;
+            for (i = 0; i < size; i++){
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+	matrixC[i][j] = sum;
+        }
+   }
+	
+   std::cout<<"The result is "<<std::endl;
+   for(i = 0; i < size; i++){
+       for( j = 0; j < size; j++){
+	  std:: cout<<matrixC[i][j]<<" ";
+	}
+   std:: cout<<std::endl;  
+   }
+}
+
+void KIJ(int size) {
+
+  int i,j,k;
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
+   for (k = 0; k < size; k++){
+        for (i = 0; i < size; i++){
+	    sum = 0;
+            for (j = 0; j < size; j++){
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+	matrixC[i][j] = sum;
+        }
+   }
+	
+   std::cout<<"The result is "<<std::endl;
+   for(i = 0; i < size; i++){
+       for( j = 0; j < size; j++){
+	  std:: cout<<matrixC[i][j]<<" ";
+	}
+   std:: cout<<std::endl;  
+   }
+}
+
+void KJI(int size) {
+
+  int i,j,k;
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
+   for (k = 0; k < size; k++){
+        for (j = 0; j < size; j++){
+	    sum = 0;
+            for (i = 0; i < size; i++){
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+	matrixC[i][j] = sum;
+        }
+   }
+	
+   std::cout<<"The result is "<<std::endl;
+   for(i = 0; i < size; i++){
+       for( j = 0; j < size; j++){
+	  std:: cout<<matrixC[i][j]<<" ";
+	}
+   std:: cout<<std::endl;  
+   }
+}
+
+/* Fills matrices A and B with random doubles by taking random ints 100<x<500 divided by
+ random ints where 1<x<100 */
 void fillMatrices(double matrixA[][3], double matrixB[][3], int size) {
    
    int i, j, k;
@@ -50,8 +202,8 @@ void fillMatrices(double matrixA[][3], double matrixB[][3], int size) {
    std::cout<<"Matrix one"<<std::endl;
    for (i = 0; i < size; i++) {
        for(j = 0; j < size; j++) {
-          r1 = rand() % 500 + 300;
-	  r1 /= rand() % 300 + 1;
+          r1 = rand() % 500 + 100;
+	  r1 /= rand() % 100 + 1;
           matrixA[i][j] = r1;
 	  std::cout<<r1<<" ";;
         }
@@ -61,15 +213,29 @@ void fillMatrices(double matrixA[][3], double matrixB[][3], int size) {
     std::cout<<"Matrix two"<<std::endl;
     for (i = 0; i < size; i++) {
        for(j = 0; j < size; j++) {
-          r2 = rand() % 500 + 300;
-	  r2 /= rand() % 300 + 1;
+          r2 = rand() % 500 + 100;
+	  r2 /= rand() % 100 + 1;
           matrixB[i][j] = r2;
 	  std::cout<<r2<<" ";
         }
     std:: cout<<std::endl;
     }
 }
-int main () {
-  naiveMMM(3);
 
+
+int main () {
+  int size = 3; 
+
+  std::cout<<"IJK"<<std::endl;
+  IJK(size);
+  std::cout<<"IKJ"<<std::endl;
+  IKJ(size);
+  std::cout<<"JIK"<<std::endl;
+  JIK(size);
+  std::cout<<"JKI"<<std::endl;
+  JKI(size); 
+  std::cout<<"KIJ"<<std::endl;
+  KIJ(size);
+  std::cout<<"KJI"<<std::endl; 
+  KJI(size);
 }
