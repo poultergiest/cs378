4ab342c85cca77451fb00a7e56fdac07d1d1eb32
Ethan Poulter <ethan@cs.utexas.edu>
2013-09-18 17:43:19 -0500

    Fixed naive Matrix Multiply.
    I'm having some issues really getting rid of all these implicit
    declarations of size (i.e. using a variable "size" instead of just
    saying "3" for the size. If you can fix this, please do so. However,
    IJK does work. Now I will impliment the other ones.

--- MissRatio.cpp
+++ MissRatio.cpp
@@ -8,40 +8,29 @@ stuff do WHATEVER YOU WANT man. This is just something to start with.
 ***Created and Owned by Ethan Poulter and Jake Wilke*** 
 */
 
+/* Function Declarations */
+void naiveMMM(int size);
+void fillMatrices(double matrixA[][3], double matrixB[][3], int size);
 
+
+/* Implementation of the first naive I, J, K matrix multiply */
 void naiveMMM(int size) {
 
   int i,j,k;
-  double r1, r2;
-  int matrixA [size][size];
-  int matrixB [size][size];
-  int matrixC [size][size];
-  
-   std::cout<<"Matrix one"<<std::endl;
-   for (i = 0; i < size; i++) {
-       for(j = 0; j < size; j++) {
-          r1 = rand() % 2 + 1;
-          matrixA[i][j] = r1;
-	  std::cout<<r1<<" ";;
-        }
-    std:: cout<<std::endl;
-    }
-    
-    std::cout<<"Matrix two"<<std::endl;
-    for (i = 0; i < size; i++) {
-       for(j = 0; j < size; j++) {
-          r2 = rand() % 2 + 1;
-          matrixB[i][j] = r2;
-	  std::cout<<r2<<" ";;
-        }
-    std:: cout<<std::endl;
-    }
-   
+  double sum;
+  double matrixA [size][3];
+  double matrixB [size][3];
+  double matrixC [size][size];
+ 
+  fillMatrices(matrixA, matrixB, size); 
+
    for (i = 0; i < size; i++){
         for (j = 0; j < size; j++){
+	    sum = 0;
             for (k = 0; k < size; k++){
-                matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+                sum += matrixA[i][k] * matrixB[k][j];
             }
+	matrixC[i][j] = sum;
         }
    }
 	
@@ -53,7 +42,33 @@ void naiveMMM(int size) {
    std:: cout<<std::endl;  
    }
 }
-
+void fillMatrices(double matrixA[][3], double matrixB[][3], int size) {
+   
+   int i, j, k;
+   double r1, r2;
+   
+   std::cout<<"Matrix one"<<std::endl;
+   for (i = 0; i < size; i++) {
+       for(j = 0; j < size; j++) {
+          r1 = rand() % 500 + 300;
+	  r1 /= rand() % 300 + 1;
+          matrixA[i][j] = r1;
+	  std::cout<<r1<<" ";;
+        }
+    std::cout<<std::endl;
+    }
+    
+    std::cout<<"Matrix two"<<std::endl;
+    for (i = 0; i < size; i++) {
+       for(j = 0; j < size; j++) {
+          r2 = rand() % 500 + 300;
+	  r2 /= rand() % 300 + 1;
+          matrixB[i][j] = r2;
+	  std::cout<<r2<<" ";
+        }
+    std:: cout<<std::endl;
+    }
+}
 int main () {
   naiveMMM(3);
 
