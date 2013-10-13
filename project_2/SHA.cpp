cc7fa54b3da182ce81c3b5aab60cd4b51cf4265c
Jacob Wilke <jake.wilke@gmail.com>
2013-10-13 14:08:52 -0500

    fix to makefile

--- project_2/SHA.cpp
+++ project_2/SHA.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <time.h>
 
+using namespace std;
 
 timespec diff(timespec start, timespec end)
 {
@@ -27,29 +28,56 @@ timespec diff(timespec start, timespec end)
 }*/
 
 /* Professor Implementation of SHA */
-/*void sha256(unsigned char output[SHA256_DIGEST_LENGTH], const char* input, int len) {
+void prof_sha256(unsigned char output[SHA256_DIGEST_LENGTH], const char* input, int len) {
     unsigned char hash1[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256,sha256_pass2;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, input, len);
     SHA256_Final(hash1, &sha256);
 
-    // printf("first pass: ");
-    // dumpHash(hash1);
-    // printf("\n");
+     // printf("first pass: ");
+     // dumpHash(hash1);
+     // printf("\n");
 
     SHA256_Init(&sha256_pass2);
     SHA256_Update(&sha256_pass2, hash1, SHA256_DIGEST_LENGTH);
     SHA256_Final(output, &sha256_pass2);
 
-    // printf("second pass: ");
-    // dumpHash(output);
-    // printf("\n");
-}*/
+     // printf("second pass: ");
+     // dumpHash(output);
+     // printf("\n");
+}
 
-void sha256(unsigned char output[SHA256_DIGEST_LENGTH], const char* input, int len){
-    unsigned int h0, h1, h2, h3, h4, h5, h6, h7, h8;
-    
+void xsha256(unsigned char output[SHA256_DIGEST_LENGTH], const char* input, int len){
+
+  unsigned int h0, h1, h2, h3, h4, h5, h6, h7, h8;
+  h0 = 0x6a09e667;
+  h1 = 0xbb67ae85;
+  h2 = 0x3c6ef372;
+  h3 = 0xa54ff53a;
+  h4 = 0x510e527f;
+  h5 = 0x9b05688c;
+  h6 = 0x1f83d9ab;
+  h7 = 0x5be0cd19;
+
+  unsigned int k [] = {
+  0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+  0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+  0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+  0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+  0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+  0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+  0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+  0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+  };
+
+  int length_before_pre_processing = strlen(input) * 8;
+
+  input += '1';
+
+  while(strlen(input)*sizeof(char)*8 % 512 != 448) {
+    input += '0';
+  }
 }
 
 
@@ -64,12 +92,12 @@ int main() {
   for (int k = 0; k < 256; ++k) {
     for (int j = 0; j < 256; ++j) {
       for (int i = 0; i < 256; ++i) {
-        src[60] = i;
+        src[60] = i; 
         src[61] = j;
         src[62] = k;
         //printf("testing %d, %d, %d\n", i, j, k);
-        //sha256(buffer, src, 128);
-        //    printf("%s\n", buffer);
+        //prof_sha256(buffer, src, 128);
+        //printf("%s\n", buffer);
       }
     }
   }
