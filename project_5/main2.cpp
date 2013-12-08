563d7f65e690f2c30e4ac9c313fa7c2575efef41
Jacob Wilke <jake.wilke@gmail.com>
2013-12-07 20:16:41 -0600

    original project files

---
+++ project_5/main2.cpp
@@ -0,0 +1,19 @@
+// main2.cpp
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+
+#include "utils.h"
+//#include "CController.h"
+//#include "CTimer.h"
+#include "resource.h"
+#include "CParams.h"
+
+using namespace std;
+
+int main() {
+	cout << "hey ethan" << endl;
+
+	return 0;
+}
